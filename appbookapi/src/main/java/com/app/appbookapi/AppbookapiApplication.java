57935b095bfb4c56384b9071127cf282e2210c63
Aletarget <alejomora1205@gmail.com>
2024-11-10 18:01:21 -0500

    aplicacion base

---
+++ appbookapi/src/main/java/com/app/appbookapi/AppbookapiApplication.java
@@ -0,0 +1,13 @@
+package com.app.appbookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbookapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbookapiApplication.class, args);
+	}
+
+}
---
+++ appbookapi/src/test/java/com/app/appbookapi/AppbookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.appbookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
